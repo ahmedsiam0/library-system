ed9d727d76a6323f5d3d7f4ec6c3d016c2be932b
Ahmed Siam <aahs.coder@gmail.com>
2023-11-30 14:42:43 +0200

    Change Main filename

--- src/main/java/com/asu/librarysystem/MainApplication.java
+++ src/main/java/com/asu/librarysystem/MainApplication.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 */
-public class HelloApplication /* extends Application */ {
+public class MainApplication /* extends Application */ {
     /*
     @Override
     public void start(Stage stage) throws IOException {
